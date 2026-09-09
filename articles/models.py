415fca7a7ca246594e70eb4ed34677fc3897a750
Zealous242 <yasum.abbas137@gmail.com>
2026-09-09 16:42:29 +0100

    Create a Django category model with name, slug & description

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
